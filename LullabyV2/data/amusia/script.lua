2204f6bd6751721cdf437e0824a0ea953588e2a6
FelixBand <felixband@gmail.com>
2026-09-21 20:25:46 +0200

    amusia - implement note dancing logic

--- LullabyV2/data/amusia/script.lua
+++ LullabyV2/data/amusia/script.lua
@@ -7,18 +7,6 @@ local strumShakeTime = 0
 local strumShakeDuration = 0.1
 local strumShakeActive = false
 
-local strumBaseX = {}
-local strumBaseY = {}
-
-function shakeOpponentNote(noteData)
-	shakeNote = noteData
-	shakeTime = 0.2
-
-	local strum = 'opponentStrums.members[' .. noteData .. ']'
-	shakeBaseX = getProperty(strum .. '.x')
-	shakeBaseY = getProperty(strum .. '.y')
-end
-
 function onCreate()
 	setProperty('skipCountdown', true)
 	setProperty('cameraSpeed', 100)
@@ -36,15 +24,6 @@ function onCreate()
 	end
 end
 
-function onCreatePost()
-	local strumCount = getProperty('opponentStrums.length')
-
-	for i = 0, strumCount - 1 do
-		strumBaseX[i] = getPropertyFromGroup('opponentStrums', i, 'x')
-		strumBaseY[i] = getPropertyFromGroup('opponentStrums', i, 'y')
-	end
-end
-
 function onSongStart()
 	setProperty('camZooming', true)
 	triggerEvent('Camera Follow Pos', 1150, 1100)
@@ -57,63 +36,39 @@ function onUpdate(elapsed)
 	if playsAsBF() then
 		if whichNote <= #strumTimes and getSongPosition() > strumTimes[whichNote] then
 			if mustPresses[whichNote] == false then
-				singDirection(noteDatas[whichNote])
+			singDirection(noteDatas[whichNote])
 
-				-- Start synchronized shake
-				strumShakeTime = strumShakeDuration
-				strumShakeActive = true
-			end
+			strumShakeTime = strumShakeDuration
+			strumShakeActive = true
+		end
 
 			whichNote = whichNote + 1
 		end
 	end
 
-	-- Synchronized opponent strum shake
+	-- Synchronized opponent strum shake using sprite offsets.
 	if strumShakeActive then
 		strumShakeTime = strumShakeTime - elapsed
+
+		local strumCount = getProperty('opponentStrums.length')
 
 		if strumShakeTime <= 0 then
 			strumShakeTime = 0
 			strumShakeActive = false
 
-			local strumCount = getProperty('opponentStrums.length')
-
+			-- Reset every strum to its normal offset.
 			for i = 0, strumCount - 1 do
-				setPropertyFromGroup(
-					'opponentStrums',
-					i,
-					'x',
-					strumBaseX[i]
-				)
-
-				setPropertyFromGroup(
-					'opponentStrums',
-					i,
-					'y',
-					strumBaseY[i]
-				)
+				setPropertyFromGroup('opponentStrums', i, 'offset.x', 23)
+				setPropertyFromGroup('opponentStrums', i, 'offset.y', 23)
 			end
 		else
-			-- One random offset shared by ALL strums
-			local shakeX = math.random(-3, 3)
-			local shakeY = math.random(-3, 3)
-
-			local strumCount = getProperty('opponentStrums.length')
+			-- One shared random offset for ALL opponent strums.
+			local shakeX = getRandomInt(21, 25)
+			local shakeY = getRandomInt(21, 25)
 
 			for i = 0, strumCount - 1 do
-				setPropertyFromGroup(
-					'opponentStrums',
-					i,
-					'x',
-					strumBaseX[i] + shakeX
-				)
-
-				setPropertyFromGroup(
-					'opponentStrums',
-					i,
-					'y',
-					strumBaseY[i] + shakeY
-				)
+				setPropertyFromGroup('opponentStrums', i, 'offset.x', shakeX)
+				setPropertyFromGroup('opponentStrums', i, 'offset.y', shakeY)
 			end
 		end
 	end
@@ -163,7 +118,6 @@ function getSingAnimation(noteData)
 
 	-- 5K
 	-- LEFT, DOWN, UP, UP, RIGHT
-	-- with the center lane being UP.
 	if keyCount == 5 then
 		local directions = {
 			'singLEFT',
@@ -298,20 +252,35 @@ end
 
 function onStepHit()
 	if curStep == 13 then
-		doTweenX('dadIn', 'dadGroup', getProperty('dad.x') - screenWidth, (stepCrochet / 1000) * 12, 'circInOut')
+		doTweenX(
+			'dadIn',
+			'dadGroup',
+			getProperty('dad.x') - screenWidth,
+			(stepCrochet / 1000) * 12,
+			'circInOut'
+		)
+
 	elseif curStep == 19 then
-		doTweenX('bfIn', 'boyfriendGroup', getProperty('boyfriend.x') + screenWidth, (stepCrochet / 1000) * 12, 'circInOut')
+		doTweenX(
+			'bfIn',
+			'boyfriendGroup',
+			getProperty('boyfriend.x') + screenWidth,
+			(stepCrochet / 1000) * 12,
+			'circInOut'
+		)
 	end
 
 	if curStep == 272 then
 		doTweenAlpha('staticIn', 'static', 0.25, 0.5, 'linear')
 		doTweenAlpha('redIn', 'redStatic', 0.25, 0.5, 'linear')
 	end
+
 	if curStep == 280 then
 		doTweenAlpha('redOut', 'redStatic', 0, 0.1, 'linear')
 		setProperty('static.alpha', 0)
 		doTweenAlpha('staticIn', 'static', 0.25, 0.5, 'linear')
 	end
+
 	if curStep == 288 then
 		doTweenAlpha('staticOut', 'static', 0, 0.5, 'linear')
 	end
@@ -320,36 +289,57 @@ function onStepHit()
 		doTweenAlpha('staticIn', 'static', 0.25, 0.5, 'linear')
 		doTweenAlpha('redIn', 'redStatic', 0.5, 0.5, 'linear')
 	end
+
 	if curStep == 544 then
 		doTweenAlpha('staticOut', 'static', 0.05, 0.1, 'linear')
 		doTweenAlpha('redOut', 'redStatic', 0, 0.1, 'linear')
 	end
 
 	if curStep == 728 then
-        doTweenX('chromUp', 'chromaticController', 15, 0.677, 'cubeIn')
+		doTweenX('chromUp', 'chromaticController', 15, 0.677, 'cubeIn')
 		doTweenX('pincUp', 'pincushionController', 0.5, 0.677, 'cubeIn')
-        doTweenAlpha('staticIn', 'static', 0.25, 0.5, 'linear')
+		doTweenAlpha('staticIn', 'static', 0.25, 0.5, 'linear')
 		doTweenAlpha('redIn', 'redStatic', 0.25, 0.5, 'linear')
-    end
-    if curStep == 736 then
-        cancelTween('chromUp')
-        setProperty('chromaticController.x', 0)
+	end
+
+	if curStep == 736 then
+		cancelTween('chromUp')
+		setProperty('chromaticController.x', 0)
 		setProperty('pincushionController.x', 0)
 		doTweenAlpha('staticOut', 'static', 0.05, 0.1, 'linear')
-    end
+	end
 
 	if curStep == 791 then
-        doTweenX('dadSlideRight', 'dadGroup', getProperty('dadGroup.x') + 1000, 0.75, 'circIn')
+		doTweenX('dadSlideRight', 'dadGroup', getProperty('dadGroup.x') + 1000, 0.75, 'circIn')
+
 		doTweenX('bfSlideLeft', 'boyfriendGroup', getProperty('boyfriend.x') - 1000, 0.75, 'circIn')
 
 		doTweenX('plateLright', 'plateL', getProperty('plateL.x') + 1000, 0.75, 'circIn')
+
 		doTweenX('plateRleft', 'plateR', getProperty('plateR.x') - 1000, 0.75, 'circIn')
 
 		setShaderFloat('background', 'prob', 1)
-        setShaderFloat('background', 'vignetteIntensity', 1)
+		setShaderFloat('background', 'vignetteIntensity', 1)
 
 		triggerEvent('Camera Follow Pos', '1050', '1150')
-    end
+	end
+
+	-- Swap the strums
+	if curStep == 800 then
+		if not middlescroll then
+			local strumCount = getProperty('opponentStrums.length')
+
+			for i = 0, strumCount - 1 do
+				noteTweenX('opponentStrumTween' .. i, i, _G['defaultPlayerStrumX' .. i], 1, 'cubeInOut')
+				noteTweenX('playerStrumTween' .. i, i + strumCount, _G['defaultOpponentStrumX' .. i], 1, 'cubeInOut')
+				
+				noteTweenAngle('noteSpin' .. i, i, 360, 1, 'cubeInOut')
+				noteTweenAngle('noteSpin' .. i + strumCount, i + strumCount, 360, 1, 'cubeInOut')
+			end
+
+		end
+	end
+
 	if curStep == 804 then
 		setProperty('dadGroup.x', 160)
 		setProperty('dadGroup.y', 482)
@@ -357,54 +347,171 @@ function onStepHit()
 		setProperty('boyfriendGroup.x', 1540)
 		setProperty('boyfriendGroup.y', 680)
 
-        doTweenX('dadSlideBack', 'dadGroup', getProperty('dadGroup.x') + 1000, 0.75, 'quartOut')
-		doTweenX('bfSlideBack', 'boyfriendGroup', getProperty('boyfriendGroup.x') - 1000, 0.75, 'quartOut')
+		doTweenX(
+			'dadSlideBack',
+			'dadGroup',
+			getProperty('dadGroup.x') + 1000,
+			0.75,
+			'quartOut'
+		)
 
-		doTweenX('plateLback', 'plateL', getProperty('plateL.x') - 1000, 0.75, 'quartOut')
-		doTweenX('plateRback', 'plateR', getProperty('plateR.x') + 1000, 0.75, 'quartOut')
+		doTweenX(
+			'bfSlideBack',
+			'boyfriendGroup',
+			getProperty('boyfriendGroup.x') - 1000,
+			0.75,
+			'quartOut'
+		)
+
+		doTweenX(
+			'plateLback',
+			'plateL',
+			getProperty('plateL.x') - 1000,
+			0.75,
+			'quartOut'
+		)
+
+		doTweenX(
+			'plateRback',
+			'plateR',
+			getProperty('plateR.x') + 1000,
+			0.75,
+			'quartOut'
+		)
 
 		setProperty('healthBar.flipX', true)
-    end
+	end
+
 	if curStep == 809 then
 		triggerEvent('Camera Follow Pos', '', '')
 		doTweenX('chromUp', 'chromaticController', 15, 0.677, 'cubeIn')
 		doTweenX('pincUp', 'pincushionController', 0.3, 0.677, 'cubeIn')
-    end
+	end
+
 	if curStep == 1296 then
 		doTweenX('chromOff', 'chromaticController', 0, 1, 'cubeIn')
 		doTweenX('pincOff', 'pincushionController', 0, 1, 'cubeIn')
 		doTweenAlpha('redIn', 'redStatic', 0.75, 0.667, 'cubeIn')
 		doTweenAlpha('staticIn', 'static', 0.1, 0.667, 'linear')
-    end
+	end
+
 	if curStep == 1312 then
 		doTweenAlpha('redOut', 'redStatic', 0.25, 0.667, 'cubeIn')
-    end
+	end
+
 	if curStep == 2000 then
 		doTweenAlpha('redIn', 'redStatic', 0.75, 2, 'cubeIn')
 		doTweenAlpha('staticIn', 'static', 0.25, 1, 'cubeIn')
-    end
+	end
+
 	if curStep == 2052 then
 		doTweenAlpha('camOut', 'camGame', 0, 0.75, 'cubeIn')
 		doTweenAlpha('hudOut', 'camHUD', 0, 0.75, 'cubeIn')
 		doTweenAlpha('redOut', 'redStatic', 0, 2, 'cubeIn')
-    end
+	end
+
 	if curStep == 2064 then
 		setProperty('background.visible', false)
 		doTweenAlpha('questionareIn', 'questionare', 1, 3, 'linear')
 		doTweenAlpha('wigglesIn', 'wigglesEnd', 1, 3, 'linear')
-    end
+	end
+
 	if curStep == 2103 then
 		doTweenAlpha('staticOut', 'static', 0, 1, 'linear')
-    end
+	end
+end
+
+function onSectionHit() getPropertyFromGroup('opponentStrums', i, 'angle')
+	if curSection >= 82 and curSection % 2 == 0 then
+		for i = 0, getProperty('opponentStrums.length') - 1 do
+			noteTweenAngle('noteSpin' .. i, i, getPropertyFromGroup('opponentStrums', i, 'angle') + 360, 1, 'cubeInOut')
+			noteTweenAngle('noteSpin' .. i + getProperty('opponentStrums.length'), i + getProperty('opponentStrums.length'), getPropertyFromGroup('playerStrums', i, 'angle') + 360, 1, 'cubeInOut')
+		end
+	end
+end
+
+function waveStrumsY(group, defaultPrefix)
+	for i = 0, getProperty(group .. '.length') - 1 do
+		local wave = math.sin(
+			(getSongPosition() / (stepCrochet * 8)) * math.pi
+				+ (i * 75)
+		) * 30
+
+		setPropertyFromGroup(
+			group,
+			i,
+			'y',
+			_G[defaultPrefix .. i] + wave
+		)
+	end
+end
+
+function waveStrumsX(group, defaultPrefix)
+	local wave = math.sin(
+		(getSongPosition() / (stepCrochet * 16)) * math.pi
+	) * 75
+
+	for i = 0, getProperty(group .. '.length') - 1 do
+		setPropertyFromGroup(
+			group,
+			i,
+			'x',
+			_G[defaultPrefix .. i]
+				+ wave
+		)
+	end
+end
+
+function updateStrumWaves()
+	if curStep >= 800 then
+		waveStrumsY('opponentStrums', 'defaultOpponentStrumY')
+		waveStrumsY('playerStrums', 'defaultPlayerStrumY')
+	end
+
+	if curSection >= 82 then
+		if middlescroll then
+			waveStrumsX('opponentStrums', 'defaultOpponentStrumX')
+			waveStrumsX('playerStrums', 'defaultPlayerStrumX')
+		else
+			waveStrumsX('opponentStrums', 'defaultPlayerStrumX')
+			waveStrumsX('playerStrums', 'defaultOpponentStrumX')
+		end
+	end
+end
+
+function updateHealthIcons()
+	if curStep >= 804 then
+		setProperty(
+			'iconP1.x',
+			getProperty('healthBar.x')
+				+ (
+					getProperty('healthBar.width') * getProperty('healthBar.percent') * 0.01
+					+ (150 * getProperty('iconP1.scale.x') - 150) / 2
+					- 26
+				)
+				- 110
+		)
+
+		setProperty('iconP1.origin.x', 240)
+		setProperty('iconP1.flipX', true)
+
+		setProperty(
+			'iconP2.x',
+			getProperty('healthBar.x')
+				+ (
+					getProperty('healthBar.width') * getProperty('healthBar.percent') * 0.01
+					- (150 * getProperty('iconP2.scale.x')) / 2
+					- 26 * 2
+				)
+				+ 110
+		)
+
+		setProperty('iconP2.origin.x', -100)
+		setProperty('iconP2.flipX', true)
+	end
 end
 
 function onUpdatePost()
-	if curStep >= 804 then
-		setProperty('iconP1.x',getProperty('healthBar.x') + ((getProperty('healthBar.width') *        getProperty('healthBar.percent') * 0.01) + (150 * getProperty('iconP1.scale.x') - 150) / 2 - 26) - 110)
-		setProperty('iconP1.origin.x',240)
-		setProperty('iconP1.flipX',true)
-		setProperty('iconP2.x',getProperty('healthBar.x') + ((getProperty('healthBar.width') * getProperty('healthBar.percent') * 0.01) - (150 * getProperty('iconP2.scale.x')) / 2 - 26 * 2) + 110)
-		setProperty('iconP2.origin.x',-100)
-		setProperty('iconP2.flipX',true)
-	end
+	updateStrumWaves()
+	updateHealthIcons()
 end
